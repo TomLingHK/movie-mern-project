80c891494613f00218511e8561c666f029a2a376
TOM-GIGABYTE\user <bxtom0@gmail.com>
2024-06-16 21:28:41 +0800

    dev: [General] Add cors in node

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -2,11 +2,20 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 
 connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: `http://localhost:${port}`,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    }
+))
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactsRoutes"));
