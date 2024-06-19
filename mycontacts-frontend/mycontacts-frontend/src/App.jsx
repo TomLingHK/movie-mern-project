b7647fac388e3309493f5ce48d78f365b04fbe3c
TOM-GIGABYTE\user <bxtom0@gmail.com>
2024-06-19 21:17:47 +0800

    dev: [Frontend] Add routes

--- mycontacts-frontend/mycontacts-frontend/src/App.jsx
+++ mycontacts-frontend/mycontacts-frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import CreateContact from './pages/CreateContact';
+import DeleteContact from './pages/DeleteContact';
+import GetContacts from './pages/GetContacts';
 
 const App = () => {
-  return (
-    <div className="bg-red-400 text-white">App</div>
-  )
+    return (
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/api/contacts' element={<CreateContact/>}/>
+            <Route path='/api/contacts/:id' element={<DeleteContact/>}/>
+            {/* <Route path='/api/contacts' element={<GetContacts/>}/> */}
+        </Routes>
+    )
 }
 
 export default App
---
+++ mycontacts-frontend/mycontacts-frontend/src/pages/GetContacts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GetContacts = () => {
+  return (
+    <div>GetContacts</div>
+  )
+}
+
+export default GetContacts
