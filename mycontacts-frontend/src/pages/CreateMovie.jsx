f044f9dcb3ea5ee175e9ebb4c6ddf3a838637716
H251 Tom <251@solmaze.net>
2024-08-05 14:21:58 +0800

    dev: [Frontend] Add component MovieForm

--- mycontacts-frontend/src/pages/CreateMovie.jsx
+++ mycontacts-frontend/src/pages/CreateMovie.jsx
@@ -4,6 +4,8 @@ import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+import MovieForm from '../components/home/MovieForm';
+
 function CreateMovie() {
     const [name, setName] = useState('');
     const [director, setDirector] = useState('');
@@ -35,94 +37,24 @@ function CreateMovie() {
             });
     }
 
-    async function handleFileUpload(e) {
-        const file = e.target.files[0];
-        const base64 = await convertToBase64(file);
-        const fileSizeInMb = bytesToMB(file.size);
-
-        if (fileSizeInMb >= 5) {
-            alert("Please select an image smaller than 5MB!");
-            return;
-        }
-
-        setImgData(base64);
-
-        function bytesToMB(bytes) {
-            return (bytes / (1024 * 1024)).toFixed(2);
-            // return (bytes / (1024 * 1024)).toFixed(2) + ' MB';
-        }
-    }
-
-    function convertToBase64(file) {
-        return new Promise((resolve, reject) => {
-            const fileReader = new FileReader();
-            fileReader.readAsDataURL(file);
-            fileReader.onload = () => {
-                resolve(fileReader.result)
-            };
-            fileReader.onerror = (error) => {
-                reject(error)
-            };
-        })
-    }
-
     return (
         <div className='p-4'>
             <BackButton/>
             <h1 className='text-3xl my-4'>Create Movie</h1>
             {loading ? <Spinner/> : ''}
-            <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
-                <div className='my-4'>
-                    <label className='text-xl mr-4 text-gray-500'>Name</label>
-                    <input 
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        className='border-2 border-gray-500 px-4 py-2 w-full'
-                    />
-                </div>
-                <div className='my-4'>
-                    <label className='text-xl mr-4 text-gray-500'>Director</label>
-                    <input 
-                        type="text"
-                        value={director}
-                        onChange={(e) => setDirector(e.target.value)}
-                        className='border-2 border-gray-500 px-4 py-2 w-full'
-                    />
-                </div>
-                <div className='my-4'>
-                    <label className='text-xl mr-4 text-gray-500'>Year</label>
-                    <input 
-                        type="text"
-                        value={year}
-                        onChange={(e) => setYear(e.target.value)}
-                        className='border-2 border-gray-500 px-4 py-2 w-full'
-                    />
-                </div>
-                <div className='my-4' >
-                    <label className='text-xl mr-4 text-gray-500'>Description</label>
-                    <textarea 
-                        type="text"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        className='border-2 border-gray-500 px-4 py-2 w-full h-48 text-xl rounded-2xl'
-                    />
-                </div>
-                <img src={imgData} alt="" />
-                <div className='my-4'>
-                    <input 
-                        type="file" 
-                        label="Image"
-                        name="myFile"
-                        id="file-upload"
-                        accept=".jpeg, .png, .jpg"
-                        onChange={(e) => handleFileUpload(e)}
-                    />
-                </div>
-                <button className='p-2 bg-sky-300 m-8' onClick={handleSaveMovie}>
-                    Save
-                </button>
-            </div>
+            <MovieForm
+                name={name}
+                setName={setName}
+                director={director}
+                setDirector={setDirector}
+                year={year}
+                setYear={setYear}
+                description={description}
+                setDescription={setDescription}
+                imgData={imgData}
+                setImgData={setImgData}
+                onSaveFormClick={handleSaveMovie}
+            />
         </div>
     )
 }
--- mycontacts-frontend/src/pages/UpdateMovie.jsx
+++ mycontacts-frontend/src/pages/UpdateMovie.jsx
@@ -4,6 +4,8 @@ import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
 
+import MovieForm from '../components/home/MovieForm';
+
 function UpdateMovie() {
     const [name, setName] = useState('');
     const [director, setDirector] = useState('');
@@ -55,94 +57,24 @@ function UpdateMovie() {
             });
     }
 
-    async function handleFileUpload(e) {
-        const file = e.target.files[0];
-        const base64 = await convertToBase64(file);
-        const fileSize = bytesToMB(file.size);
-
-        if (fileSize >= 5) {
-            alert("Please select an image smaller than 5KB!");
-            return;
-        }
-
-        setImgData(base64);
-
-        function bytesToMB(bytes) {
-            return (bytes / (1024 * 1024)).toFixed(2);
-            // return (bytes / (1024 * 1024)).toFixed(2) + ' MB';
-        }
-    }
-
-    function convertToBase64(file) {
-        return new Promise((resolve, reject) => {
-            const fileReader = new FileReader();
-            fileReader.readAsDataURL(file);
-            fileReader.onload = () => {
-                resolve(fileReader.result)
-            };
-            fileReader.onerror = (error) => {
-                reject(error)
-            };
-        })
-    }
-
     return (
         <div className='p-4'>
             <BackButton/>
             <h1 className='text-3xl my-4'>Update Movie</h1>
             {loading ? <Spinner/> : ''}
-            <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
-                <div className='my-4'>
-                    <label className='text-xl mr-4 text-gray-500'>Name</label>
-                    <input 
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        className='border-2 border-gray-500 px-4 py-2 w-full rounded-xl'
-                    />
-                </div>
-                <div className='my-4'>
-                    <label className='text-xl mr-4 text-gray-500'>Director</label>
-                    <input 
-                        type="text"
-                        value={director}
-                        onChange={(e) => setDirector(e.target.value)}
-                        className='border-2 border-gray-500 px-4 py-2 w-full rounded-xl'
-                    />
-                </div>
-                <div className='my-4'>
-                    <label className='text-xl mr-4 text-gray-500'>Year</label>
-                    <input 
-                        type="text"
-                        value={year}
-                        onChange={(e) => setYear(e.target.value)}
-                        className='border-2 border-gray-500 px-4 py-2 w-full rounded-xl'
-                    />
-                </div>
-                <div className='my-4' >
-                    <label className='text-xl mr-4 text-gray-500'>Description</label>
-                    <textarea 
-                        type="text"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        className='border-2 border-gray-500 px-4 py-2 w-full h-48 text-xl rounded-2xl'
-                    />
-                </div>
-                <img src={imgData} alt="" />
-                <div className='my-4'>
-                    <input 
-                        type="file" 
-                        label="Image"
-                        name="myFile"
-                        id="file-upload"
-                        accept=".jpeg, .png, .jpg"
-                        onChange={(e) => handleFileUpload(e)}
-                    />
-                </div>
-                <button className='p-2 bg-sky-300 m-8' onClick={handleUpdateMovie}>
-                    Save
-                </button>
-            </div>
+            <MovieForm
+                name={name}
+                setName={setName}
+                director={director}
+                setDirector={setDirector}
+                year={year}
+                setYear={setYear}
+                description={description}
+                setDescription={setDescription}
+                imgData={imgData}
+                setImgData={setImgData}
+                onSaveFormClick={handleUpdateMovie}
+            />
         </div>
     )
 }
