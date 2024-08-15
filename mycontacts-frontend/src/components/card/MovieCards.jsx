8c23979761ed03f68bcd5c2e53b1c38af7f75bb0
H251 Tom <251@solmaze.net>
2024-08-15 16:09:39 +0800

    dev: [Frontend] Add mobile styling

--- mycontacts-frontend/src/components/card/MovieCards.jsx
+++ mycontacts-frontend/src/components/card/MovieCards.jsx
@@ -2,7 +2,7 @@ import MovieSingleCard from "./MovieSingleCard";
 
 function MovieCards({ movies, curSortedType, isAscending }) {
   return (
-    <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 mx-2 overflow-auto h-[88%] w-full'>
+    <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 mx-2 overflow-auto h-[88%] w-full'>
         {movies.map((movie, index) => 
             <MovieSingleCard key={`${movies.length}_${curSortedType}_${isAscending}_${movie._id}`} movie={movie} index={index} movies={movies} />
         )}
--- mycontacts-frontend/src/components/home/MovieModal.jsx
+++ mycontacts-frontend/src/components/home/MovieModal.jsx
@@ -5,15 +5,15 @@ import { BiUserCircle } from "react-icons/bi";
 function MovieModal({ movie, onClose }) {
     return (
         <div
-            className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
+            className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center flex-col xl:flex-row"
             onClick={onClose}
         >
             <div className="bg-slate-300 m-4 rounded-xl">
-                <img src={movie?.imgData} className="m-4 h-[300px] w-[200px]" alt="" />
+                <img src={movie?.imgData} className="m-4 w-[568px] xl:w-auto xl:h-[368px]" alt="" />
             </div>
             <div
                 onClick={(event) => event.stopPropagation()}
-                className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative"
+                className="w-[600px] h-[300px] xl:h-[400px] bg-white rounded-xl p-6 flex flex-col relative"
             >
                 <AiOutlineClose
                     className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
@@ -37,11 +37,7 @@ function MovieModal({ movie, onClose }) {
                     ?
                         movie.description
                     :
-                        `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                        Praesentium numquam distinctio earum maiores consequatur?
-                        Neque rem distinctio illum est labore natus, ad nesciunt
-                        voluptate ducimus corporis cum aperiam quis ipsam, eligendi
-                        ratione eaque obcaecati fugiat, autem odit totam at atque!`
+                        "This movie has no description."
                     }
                 </p>
             </div>
